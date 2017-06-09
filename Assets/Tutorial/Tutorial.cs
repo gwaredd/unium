602edab7d20250f2a9602627727b8e03c48dcf8a
Gwaredd Mountain <gwaredd.mountain@preloaded.com>
2017-06-09 17:24:18 +0100

    Added tutorial scene wioh automatic browser opening

---
+++ Assets/Tutorial/Tutorial.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+	void Start()
+    {
+        System.Diagnostics.Process.Start( "http://localhost:8342/tutorial/index.html" );
+	}
+}
+

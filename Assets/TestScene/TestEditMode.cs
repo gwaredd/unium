7f65def37db12e395316f7488be249f4fff03301
Roger Braunstein <roger@partlyhuman.com>
2018-07-11 18:08:33 -0700

    GQL works amazing in edit mode, we have a GQL inspector for crafting and testing complex GQL queries. However, new fix to show GameObjects in DoNotDestroyOnLoad threw error in edit mode. This fixes it. Also adding a test for edit-mode GQL.

---
+++ Assets/TestScene/TestEditMode.cs
@@ -0,0 +1,54 @@
+﻿using gw.gql;
+using gw.proto.utils;
+using gw.unium;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class TestEditMode : MonoBehaviour
+{
+    public static string ExecuteGQL(string queryString)
+    {
+        var query = new Query(queryString, Unium.Root).Select();
+        return JsonReflector.Reflect(query.Execute());
+    }
+}
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(TestEditMode))]
+public class TestEditModeInspector : Editor
+{
+    private string myQuery = "/scene/";
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        myQuery = EditorGUILayout.TextField("Custom GQL", myQuery);
+        if (GUILayout.Button("Execute"))
+        {
+            LogGQLQuery(myQuery);
+        }
+
+        if (GUILayout.Button("Samples"))
+        {
+            LogSampleGQLQueries();
+        }
+    }
+
+    private void LogGQLQuery(string queryString)
+    {
+        Debug.LogFormat("Query <color=orange>{0}</color>\n{1}", queryString, TestEditMode.ExecuteGQL(queryString));
+    }
+
+    private void LogSampleGQLQueries()
+    {
+        string[] sampleQueries = {"/scene/*.name", "/scene//*.Canvas.name", "/scene//[tag='Pickup']"};
+        foreach (var query in sampleQueries)
+        {
+            LogGQLQuery(query);
+        }
+    }
+}
+#endif
